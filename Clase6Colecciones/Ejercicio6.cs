e1eb16b99dec5ae221f1831a831b59a561fe0cbd
Adriano Vazquez <adriano11vazquez@gmail.com>
2025-10-08 13:27:08 -0300

    C6. E7. Maquina Expendedora Parte 2

--- Clase6Colecciones/Ejercicio6.cs
+++ Clase6Colecciones/Ejercicio6.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entidades;
+using Producto = Entidades.Producto;
 using Utilidades;
 
 namespace Clase6Colecciones
@@ -14,9 +15,14 @@ namespace Clase6Colecciones
 
         public void Ejecutar()
         {
-             Dictionary<int, string> _maquinaExpendedora = new();
+            #region PRIMERA PARTE 
+
+            /*
+             * Dictionary<int, string> _maquinaExpendedora = new();
                 int code;
                 string p = " ";
+
+            //Lleno el Diccionario
             for (int i = 0; i < 5; i++)
             {
                 Console.WriteLine("Codigo Del Producto : ");
@@ -31,12 +37,7 @@ namespace Clase6Colecciones
                     _maquinaExpendedora.Add(code, p);
             }
 
-            /*StringBuilder sb = new();
-            foreach(KeyValuePair <int,string> productos in _maquinaExpendedora)
-            {
-                sb.AppendLine($"Posicion : {productos.Key} Nombre : {productos.Value}");
-            }
-            */
+            //Muestro productos disponibles y el usuario los selecciona.
             do
             {
 
@@ -49,7 +50,50 @@ namespace Clase6Colecciones
                  Console.WriteLine($"PRODCUTOS DISPONIBLES LUEGO DE SELECCIONAR : \n{Mostrar(_maquinaExpendedora)}");
 
                  Console.WriteLine("Desea Seguir seleccionando productos? s/n");
+            } while (Validador.ValidarRespuesta());*/
+            #endregion
+
+            #region Refactorizacion Dictionary<int,Producto>
+            Dictionary<int, Producto> _maquinaExpendedora = new();
+            int code;
+            string p = " ";
+
+            //Lleno el Diccionario
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Codigo Del Producto : ");
+                int codeKey = Validador.PedirYValidarNumeroEntero();
+                Console.WriteLine("Codigo De Barra Del Producto : ");
+                code = Validador.PedirYValidarNumeroEntero();
+                Console.WriteLine("Nombre Del Producto : ");
+                while (!Validador.ValidarString(p = Console.ReadLine()))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Nombre Del Producto : ");
+
+                }
+                Console.WriteLine("Precio Del Producto : ");
+                double precio = (double)Validador.PedirYValidarNumeroEntero();
+                Producto prod = new(code, p, precio);
+                _maquinaExpendedora.Add(codeKey,prod);
+            }
+
+            //Muestro productos disponibles y el usuario los selecciona.
+            do
+            {
+
+                Console.Clear();
+                Console.WriteLine($"PRODCUTOS DISPONIBLES : \n{Mostrar(_maquinaExpendedora)}");
+
+                Console.WriteLine("Digite la posicion y seleccione un producto : ");
+                int option = Validador.PedirYValidarNumeroEntero();
+                _maquinaExpendedora.Remove(option);
+                Console.WriteLine($"PRODCUTOS DISPONIBLES LUEGO DE SELECCIONAR : \n{Mostrar(_maquinaExpendedora)}");
+
+                Console.WriteLine("Desea Seguir seleccionando productos? s/n");
             } while (Validador.ValidarRespuesta());
+            
+            #endregion
         }
 
         private string Mostrar(Dictionary<int,string> maquinaExpendedora)
@@ -60,6 +104,15 @@ namespace Clase6Colecciones
                 sb.AppendLine($"Posicion : {productos.Key} Nombre : {productos.Value.ToString()}");
             }
             return sb.ToString();
+        }
+        private string Mostrar(Dictionary<int,Producto> maquinaExpendedora)
+        {
+                StringBuilder sb = new StringBuilder();
+                foreach (KeyValuePair<int, Producto> producto in maquinaExpendedora)
+                {
+                    sb.AppendLine($"Posicion : {producto.Key}\n {producto.Value.Mostrar()}\n");
+                }
+                return sb.ToString();
         }
     }
 }
